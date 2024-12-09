33a5e31d144afd64324b48e8efdd7bc61b49968a
EmanuelePozzi <s8901895z@majorana-giorgi.edu.it>
2024-12-09 09:45:16 +0100

    1.unit test

---
+++ src/main/java/Calcolatrice.java
@@ -0,0 +1,9 @@
+
+public class Calcolatrice {
+	public double somma (double...addendi) {
+		double s =0;
+		for(double addendo : addendi)
+			s= s + addendo;
+		return s;
+	}
+}
---
+++ src/test/java/TestCalcolatrice.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class TestCalcolatrice {
+
+	@Test
+	void testSomma() {
+		Calcolatrice c = new Calcolatrice();
+		
+		double res =c.somma(5, 6);
+		
+		Assert.assertEquals(11,res,0);
+	}
+
+}
